7bdcd20533cbbf5def632ab0992b85d7c00e7548
Adil Jamal <jamal.adil7860@gmail.com>
2026-05-10 17:15:32 +0530

    Server chat window UI

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.text.*;
 
 public class Server extends JFrame implements ActionListener {
 
+    JTextField text;
+    JPanel a1;
+    Box vertical = Box.createVerticalBox();
     Server() {
 
         setLayout(null);
@@ -55,7 +61,7 @@ public class Server extends JFrame implements ActionListener {
         morevert.setBounds(420, 20, 10, 25);
         p1.add(morevert);
 
-        JLabel name = new JLabel("Adil");
+        JLabel name = new JLabel("Server");
         name.setBounds(110, 15, 100, 18);
         name.setForeground(Color.WHITE);
         name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
@@ -67,11 +73,11 @@ public class Server extends JFrame implements ActionListener {
         status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
         p1.add(status);
 
-        JPanel a1 = new JPanel();
+        a1 = new JPanel();
         a1.setBounds(5, 75, 440, 570);
         add(a1);
 
-        JTextField text = new JTextField();
+        text = new JTextField();
         text.setBounds(5,655, 310, 40);
         text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
         add(text);
@@ -80,10 +86,9 @@ public class Server extends JFrame implements ActionListener {
         send.setBounds(320, 655, 123, 40);
         send.setBackground(new Color(7, 94, 84));
         send.setForeground(Color.WHITE);
+        send.addActionListener(this);
         send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
         add(send);
-
-
 
         setSize(450, 700);
         setLocation(200, 50);
@@ -93,8 +98,48 @@ public class Server extends JFrame implements ActionListener {
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed(ActionEvent ae) {
+            String out = text.getText();
 
+            JPanel p2 = formatLabel(out);
+
+            a1.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            a1.add(vertical, BorderLayout.PAGE_START);
+
+            text.setText("");
+
+            repaint();
+            invalidate();
+            validate();
+    }
+
+    public static JPanel formatLabel(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel(out);
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+
+        panel.add(time);
+
+        return panel;
     }
 
     public static void main(String[] args) {
